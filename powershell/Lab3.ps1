dbc7aa735c279470c9e5e36609bceee85ea2883a
lilgamelvr <marti2t5@mail.uc.edu>
2023-09-12 01:34:44 -0400

    Lab 3

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,17 @@
+﻿function getIP{ 
+
+    (get-netipaddress).ipv4address | Select-String "192*" 
+
+} 
+
+$IP = getIP | Select-Object -first 1
+
+$USER = Get-LocalUser | Select-Object -first 1
+
+$DATE = Get-Date -UFormat "%A %d %B %Y"
+
+$BODY = "This machine's IP is " + $IP + " User is " + $USER + " Hostname is " + $env:computername + " PowerShell Version " + $HOST.Version.Major + " Today's Date is " + $DATE
+
+Write-Output $BODY
+
+$BODY | Out-File -FilePath .\Lab3.txt
